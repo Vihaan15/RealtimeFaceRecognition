0a05c30d4e0d58b84b56ed5eef43c7f6d651991e
Vihaan15 <73476143+Vihaan15@users.noreply.github.com>
2021-06-04 15:54:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() { 
+    canvas = createCanvas(300, 300);
+     canvas.center();
+      video = createCapture(VIDEO);
+       video.hide();
+    }
+
+    function draw(){
+        image(video, 0, 0, 300, 300);
+    fill(83,40,10);
+    stroke(0,0,0);
+    circle(0, 0, 0);
+    
+    fill(83,40,10);
+    stroke(0,0,0);
+    circle(0,0,0);
+    
+    fill(83,40,10);
+    stroke(0,0,0);
+    circle(0,0,0);
+    
+    fill(83,40,10);
+    stroke(0,0,0);
+    circle(0,0,0);
+    
+    }
